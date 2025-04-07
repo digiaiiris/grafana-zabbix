2bd520f777809cc9b08d58c7a12d2eba81747cd9
Antti-Pekka Meronen <antti-pekka.meronen@digia.com>
2025-04-07 11:57:15 +0300

    Fixed styles importing, forgot this.

--- src/module.ts
+++ src/module.ts
@@ -2,8 +2,8 @@ import { AppPlugin } from '@grafana/data';
 import { loadPluginCss } from '@grafana/runtime';
 
 loadPluginCss({
-  dark: 'plugins/iiris-zabbix-triggers-panel/styles/grafana-zabbix.dark.css',
-  light: 'plugins/iiris-zabbix-triggers-panel/styles/grafana-zabbix.light.css',
+  dark: 'plugins/iiris-zabbix-triggers-panel/styles/dark.css',
+  light: 'plugins/iiris-zabbix-triggers-panel/styles/light.css',
 });
 
 export const plugin = new AppPlugin<{}>();
